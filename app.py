2559cda92add4f8bb33acb24f60d37f187f90d0f
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-08-29 21:00:35 -0400

    abstract UserFoodService logic from Home page route

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 import requests
 import json
 
+from service import UserFoodService
+
 from datetime import datetime, timedelta
 from functools import wraps
 
@@ -75,26 +77,10 @@ def homepage():
     if not g.user:
         return render_template('home.html')
         
-    user_food_query = db.session.query(      
-                UserFood.date.label('date'),
-                func.sum(UserFood.calories).label("total_calories"),
-                ).filter_by(user_id=g.user.id)
-    # querying last 7 user food entries
-    last_7_user_food_query = user_food_query.group_by(UserFood.date).order_by(UserFood.date.desc()).limit(7).all()
-    # django: query.values_list('date', 'amount') => [(date_1, amount_1), (date_2, amount_2)]
-    # Example shape of the last_7_user_food_query: [sqlalchemy.util._collections.result((datetime.date(2021, 8, 7), 1697)), sqlalchemy.util._collections.result((datetime.date(2021, 8, 5), 1140)), sqlalchemy.util._collections.result((datetime.date(2021, 8, 4), 570)), sqlalchemy.util._collections.result((datetime.date(2021, 8, 3), 548)), sqlalchemy.util._collections.result((datetime.date(2021, 7, 10), 1118))]
+    user_food_dates_and_calories = UserFoodService.get_latest_user_food_information(g.user.id)
+    user_food_dates = user_food_dates_and_calories["user_food_dates"]
+    user_food_calories = user_food_dates_and_calories["user_food_calories"]
 
-    last_7_user_food_data = [(t[0].strftime('%m/%d/%Y'),t[1]) for t in last_7_user_food_query]
-    # Example shape of the last_7_user_food_data: [('08/07/2021', 1697), ('08/05/2021', 1140), ('08/04/2021', 570), ('08/03/2021', 548), ('07/10/2021', 1118)]
-    
-    # sort by date ascending order
-    last_7_user_food_data.sort()
-    # Example shape of the last_7_user_food_data: [('07/10/2021', 1118), ('08/03/2021', 548), ('08/04/2021', 570), ('08/05/2021', 1140), ('08/07/2021', 1697)]
-    user_food_dates, user_food_calories = [], []
-    for row in last_7_user_food_data:
-        user_food_dates.append(row[0])
-        user_food_calories.append(row[1]) 
-    
     #order by date descending first to get last 7 entries, then sort by date ascending.
     user_bmi_query = BMI.query.order_by(BMI.date.desc()).filter_by(user_id=g.user.id).limit(7).all()
 
