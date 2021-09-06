d03dde790b81be82a7cf532565c58e2f19aeaf53
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-06 16:27:05 -0400

    format with black

--- test_user_views.py
+++ test_user_views.py
@@ -63,7 +63,7 @@ class UserViewTestCase(TestCase):
             calories=570,
             img="https://spoonacular.com/recipeImages/1096010-312x231.jpg",
         )
-        db.session.add_all([add_bmi,user_food1])
+        db.session.add_all([add_bmi, user_food1])
 
         db.session.commit()
 
@@ -80,16 +80,16 @@ class UserViewTestCase(TestCase):
 
             resp = c.get("/")
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("testuser2",str(resp.data))
-            self.assertIn("canvas",str(resp.data))
-            self.assertIn(self.u2.diet_plan,str(resp.data))
-            
+            self.assertIn("testuser2", str(resp.data))
+            self.assertIn("canvas", str(resp.data))
+            self.assertIn(self.u2.diet_plan, str(resp.data))
+
     def test_users_logged_out_food_intake(self):
         """when logged out, /food-intake should show search bar to search recipes"""
         with self.client as c:
             resp = c.get("/food-intake")
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("Search recipes here.",str(resp.data))
+            self.assertIn("Search recipes here.", str(resp.data))
 
     def test_users_logged_out_food_journal(self):
         """when logged out, /food-journal should redirect to logged out home page."""
@@ -105,6 +105,4 @@ class UserViewTestCase(TestCase):
 
             resp = c.get("/food-journal")
             self.assertEqual(resp.status_code, 200)
-            self.assertIn("Egg Salad Wrap",str(resp.data))
-
-
+            self.assertIn("Egg Salad Wrap", str(resp.data))
